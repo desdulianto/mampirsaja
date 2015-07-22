6678046ea82fcbd62c89ab647c9c093b6bc05693
Des Dulianto <desdulianto@gmail.com>
2015-07-22 15:37:18 +0700

    protect home view

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -6,6 +6,10 @@ use App\Http\Controllers\Controller;
 
 class AccountController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
     public function index() {
         return view('account.index');
     }
